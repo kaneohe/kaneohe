5ed6c39c1cad5b7bc4844b124943cb5570c0351e
Jeremy Moseley <jeremymoeley@me.com>
2016-08-19 13:18:11 -0700

    Ignore file updated to include js and css

--- footer.php
+++ footer.php
@@ -30,5 +30,8 @@
 
 <?php wp_footer(); ?>
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.14.0.js'><\/script>".replace("HOST", location.hostname));
+//]]></script>
 </body>
 </html>
