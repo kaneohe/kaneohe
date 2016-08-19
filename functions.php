3d3dd39b904dbf72972c2737d9e575d1586a5ed2
Jeremy Moseley <jeremymoeley@me.com>
2016-08-19 15:32:47 -0700

    WPML language bar addition

--- functions.php
+++ functions.php
@@ -47,5 +47,8 @@ require_once( 'library/sticky-posts.php' );
 /** Configure responsive image sizes */
 require_once( 'library/responsive-images.php' );
 
+/** Add WPML support */
+require_once( 'library/wpml.php' );
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
