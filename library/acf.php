997e20ea4daa4bf90855d8fac0a5420e6ffdb1e6
Jeremy Moseley <jeremymoeley@me.com>
2016-08-26 16:33:26 -0700

    ACF JSON load file path

--- library/acf.php
+++ library/acf.php
@@ -1,4 +1,18 @@
 <?php
+	
+if ( ! function_exists( 'kaneohe_acf_save' ) ) :
+	function kaneohe_acf_save( $path ) {
+	    
+	    // update path
+	    $path = get_stylesheet_directory() . '/assets/acf-json';
+	    
+	    
+	    // return
+	    return $path;
+	    
+	}
+	add_filter('acf/settings/save_json', 'kaneohe_acf_save');
+endif;
 
 if ( ! function_exists( 'kaneohe_acf' ) ) :
 	
@@ -98,17 +112,224 @@ if ( ! function_exists( 'kaneohe_acf' ) ) :
 	add_filter( 'the_content', 'kaneohe_acf' );
 endif;
 
+/*
+if( function_exists('acf_add_local_field_group') ):
 
-if ( ! function_exists( 'kaneohe_acf_save' ) ) :
-	function kaneohe_acf_save( $path ) {
-	    
-	    // update path
-	    $path = get_template_directory() . '/acf-json';
-	    
-	    
-	    // return
-	    return $path;
-	    
-	}
-	add_filter('acf/settings/save_json', 'kaneohe_acf_save');
-endif;
+acf_add_local_field_group(array (
+	'key' => 'group_57c07f9822c07',
+	'title' => 'Flexible Content',
+	'fields' => array (
+		array (
+			'key' => 'field_57c07fa399fd9',
+			'label' => 'Content',
+			'name' => 'content',
+			'type' => 'flexible_content',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'button_label' => 'Add Content Block',
+			'min' => '',
+			'max' => '',
+			'layouts' => array (
+				array (
+					'key' => '57c07fae1d45b',
+					'name' => 'text',
+					'label' => 'Text',
+					'display' => 'table',
+					'sub_fields' => array (
+						array (
+							'key' => 'field_57c0801d99fda',
+							'label' => 'Columns',
+							'name' => 'columns',
+							'type' => 'select',
+							'instructions' => '',
+							'required' => '',
+							'conditional_logic' => '',
+							'wrapper' => array (
+								'width' => 10,
+								'class' => '',
+								'id' => '',
+							),
+							'choices' => array (
+								1 => 1,
+								2 => 2,
+								3 => 3,
+							),
+							'default_value' => array (
+								0 => 1,
+							),
+							'allow_null' => '',
+							'multiple' => '',
+							'ui' => '',
+							'ajax' => '',
+							'placeholder' => '',
+							'disabled' => 0,
+							'readonly' => 0,
+						),
+						array (
+							'key' => 'field_57c0806a99fdb',
+							'label' => 'Column 1',
+							'name' => 'column_1',
+							'type' => 'wysiwyg',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array (
+								'width' => 28,
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'tabs' => 'visual',
+							'toolbar' => 'full',
+							'media_upload' => 1,
+						),
+						array (
+							'key' => 'field_57c080af99fdd',
+							'label' => 'Column 2',
+							'name' => 'column_2',
+							'type' => 'wysiwyg',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => array (
+								array (
+									array (
+										'field' => 'field_57c0801d99fda',
+										'operator' => '==',
+										'value' => '2',
+									),
+								),
+								array (
+									array (
+										'field' => 'field_57c0801d99fda',
+										'operator' => '==',
+										'value' => '3',
+									),
+								),
+							),
+							'wrapper' => array (
+								'width' => 28,
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'tabs' => 'visual',
+							'toolbar' => 'full',
+							'media_upload' => 1,
+						),
+						array (
+							'key' => 'field_57c080ba99fde',
+							'label' => 'Column 3',
+							'name' => 'column_3',
+							'type' => 'wysiwyg',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => array (
+								array (
+									array (
+										'field' => 'field_57c0801d99fda',
+										'operator' => '==',
+										'value' => '3',
+									),
+								),
+							),
+							'wrapper' => array (
+								'width' => 28,
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'tabs' => 'visual',
+							'toolbar' => 'full',
+							'media_upload' => 1,
+						),
+					),
+					'min' => '',
+					'max' => '',
+				),
+				array (
+					'key' => '57c0811d99fdf',
+					'name' => 'gallery',
+					'label' => 'Gallery',
+					'display' => 'table',
+					'sub_fields' => array (
+						array (
+							'key' => 'field_57c0823999fe2',
+							'label' => 'Slideshow',
+							'name' => 'slideshow',
+							'type' => 'true_false',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array (
+								'width' => 10,
+								'class' => '',
+								'id' => '',
+							),
+							'message' => 'Display photos as a slideshow?',
+							'default_value' => 1,
+						),
+						array (
+							'key' => 'field_57c0813599fe0',
+							'label' => 'Gallery',
+							'name' => 'gallery',
+							'type' => 'gallery',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array (
+								'width' => 90,
+								'class' => '',
+								'id' => '',
+							),
+							'min' => '',
+							'max' => '',
+							'insert' => 'append',
+							'library' => 'all',
+							'min_width' => '',
+							'min_height' => '',
+							'min_size' => '',
+							'max_width' => '',
+							'max_height' => '',
+							'max_size' => '',
+							'mime_types' => '',
+						),
+					),
+					'min' => '',
+					'max' => '',
+				),
+			),
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'seamless',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;*/
