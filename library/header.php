5c080379ad6a0e1f6f451c127c6f34e6cc229aae
Brian Gunzenhauser <brian@boxandpixel.com>
2016-08-31 12:26:18 -0400

    Buttons 50% in mobile

--- library/header.php
+++ library/header.php
@@ -11,8 +11,8 @@ if ( ! function_exists( 'header_include' ) ) :
 				</div>
 				<div class="small-12 medium-6 columns">
 					<div class="row">
-						<div class="small-12 medium-6 columns"><?php the_field( 'column_1', 'option' ); ?></div>
-						<div class="small-12 medium-6 columns"><?php the_field( 'column_2', 'option' ); ?></div>
+						<div class="small-6 columns"><?php the_field( 'column_1', 'option' ); ?></div>
+						<div class="small-6 columns"><?php the_field( 'column_2', 'option' ); ?></div>
 					</div> <!-- .row -->
 				</div> <!-- .small-12 -->
 
