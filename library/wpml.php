262f599314aee0941c2207bf800f4eb682f0c21e
Jeremy Moseley <jeremymoeley@me.com>
2016-09-04 12:52:58 -0700

    Language bar styles removed

--- library/wpml.php
+++ library/wpml.php
@@ -4,7 +4,7 @@ if ( ! function_exists( 'kaneohe_wpml' ) ) :
 		if ( function_exists('icl_object_id') ) : ?>
 			<div class="languages">
 				<div class="row">
-					<div class="small-12"><?php do_action('wpml_add_language_selector'); ?></div>
+					<div class="small-12 columns"><?php do_action('wpml_add_language_selector'); ?></div>
 				</div>
 			</div>
 		<?php endif;
