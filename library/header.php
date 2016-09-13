deb55bd6b87607d6be2246ae5f4d57cc97b3b2c4
Jeremy Moseley <jeremymoeley@me.com>
2016-09-12 23:15:42 -0700

    Smaller logo area and font setup

--- library/header.php
+++ library/header.php
@@ -4,16 +4,16 @@ if ( ! function_exists( 'header_include' ) ) :
 ?>
 		<header class="header">
 			<div class="row">
-				<div class="small-12 medium-6 columns">
+				<div class="small-12 medium-4 columns">
 				<?php if ( $logo = get_field( 'logo', 'option' ) ) : ?>
 					<a href="<?php echo site_url(); ?>"><img src="<?php echo $logo['url']; ?>"></a>
 				<?php endif; ?>
 				</div>
-				<div class="small-12 medium-6 columns">
-					<div class="row">
-						<div class="small-12 medium-6 columns"><?php the_field( 'column_1', 'option' ); ?></div>
-						<div class="small-12 medium-6 columns"><?php the_field( 'column_2', 'option' ); ?></div>
-					</div> <!-- .row -->
+				<div class="small-12 medium-4 columns">
+					<?php the_field( 'column_1', 'option' ); ?>
+				</div>
+				<div class="small-12 medium-4 columns">
+					<?php the_field( 'column_2', 'option' ); ?>
 				</div> <!-- .small-12 -->
 
 			</div>
