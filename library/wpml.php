c5505c0bacdc32a1891ab0b9238dc82f790e3fc6
Jeremy Moseley <jeremymoeley@me.com>
2016-09-13 16:06:30 -0700

    Hamburger above language bar

--- library/wpml.php
+++ library/wpml.php
@@ -4,7 +4,11 @@ if ( ! function_exists( 'kaneohe_wpml' ) ) :
 		if ( function_exists('icl_object_id') ) : ?>
 			<div class="languages">
 				<div class="row">
-					<div class="small-12 columns"><?php do_action('wpml_add_language_selector'); ?></div>
+					<div class="title-bar" data-responsive-toggle="site-navigation">
+						<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+					</div>
+					<?php do_action('wpml_add_language_selector'); ?>
+					
 				</div>
 			</div>
 		<?php endif;
