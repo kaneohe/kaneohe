a2be207e3474dcfe14f8c0f1f7f7312b453ceff0
Jeremy Moseley <jeremymoeley@me.com>
2016-09-28 14:23:40 -0700

    Update theme options slug

--- library/acf.php
+++ library/acf.php
@@ -4,7 +4,7 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
 		'page_title' 	=> 'Theme General Settings',
 		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'theme-general-settings',
+		'menu_slug' 	=> 'theme-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
@@ -12,13 +12,13 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Header Settings',
 		'menu_title'	=> 'Header',
-		'parent_slug'	=> 'theme-general-settings',
+		'parent_slug'	=> 'theme-settings',
 	));
 
 	acf_add_options_sub_page(array(
 		'page_title' 	=> 'Theme Sidebar Settings',
 		'menu_title'	=> 'Sidebar',
-		'parent_slug'	=> 'theme-general-settings',
+		'parent_slug'	=> 'theme-settings',
 	));
 
 }
