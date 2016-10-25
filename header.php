6a8d9bb5cdb54e8f346e6da97220a2a16f9f290f
Jeremy Moseley <jeremymoeley@me.com>
2016-10-24 18:41:34 -0700

    Version bump with header image

--- header.php
+++ header.php
@@ -25,6 +25,8 @@
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php endif; ?>
 
+	<div class="header-with-nav">
+
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header id="masthead" class="site-header" role="banner">
@@ -51,6 +53,8 @@
 			</div><!-- .small-12 -->
 		</div><!-- .row -->
 	</header>
+
+	</div><!-- .header-with-nav -->
 
 	<section class="container">
 		<?php do_action( 'foundationpress_after_header' );
